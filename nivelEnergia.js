d3e0557326efca6ea9c06197861b44588588715c
Glenilson Santil <49415313+GlenilsonSantil@users.noreply.github.com>
2022-12-20 16:41:00 -0300

    Adicionado arquivo nivelEnergia
    Desafio em JS de função IF.

---
+++ nivelEnergia.js
@@ -0,0 +1,20 @@
+// TODO: complete os espaços em branco com sua solução para o problema
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let input = gets();
+
+let qtdEntradas = parseInt(input);
+let nivelEnergia;
+
+//Resolva o desafios nos espaços em branco
+for(let i = 0; i < qtdEntradas; i++){
+    input = gets();
+    nivelEnergia = parseInt(input);
+    
+    if(nivelEnergia <= 8000){
+        print("Inseto!")
+    }else{
+        print("Mais de 8000!")
+    }
+}
